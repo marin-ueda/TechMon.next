c5ade5a7b33ea1b2871267403e8c3596bea30472
Marin Ueda <marin96nym@gmail.com>
2020-05-17 22:34:11 +0900

    Initial Commit

---
+++ TechMon.next/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TechMon.next
+//
+//  Created by 植田真梨 on 2020/05/17.
+//  Copyright © 2020 Ueda Maririn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TechMon.nextTests/TechMon_nextTests.swift
@@ -0,0 +1,34 @@
+//
+//  TechMon_nextTests.swift
+//  TechMon.nextTests
+//
+//  Created by 植田真梨 on 2020/05/17.
+//  Copyright © 2020 Ueda Maririn. All rights reserved.
+//
+
+import XCTest
+@testable import TechMon_next
+
+class TechMon_nextTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
